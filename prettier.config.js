9d1e0cd00738d8e0a79af4ae30e04b0c539b94f2
M.Dmitriev <holder.md.1@gmail.com>
2023-03-19 23:02:29 +0300

    added color theme

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  printWidth: 80,
+  printWidth: 120,
   tabWidth: 2,
   semi: false,
   singleQuote: true,
--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import { Suspense } from 'react'
+import React, { Suspense } from 'react'
 import { Route, Routes } from 'react-router'
 
 import { routeConfig } from './util/routes'
+import { createStyles } from '@theme'
 
 const App = () => {
+  const c = useStyles()
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        {Object.values(routeConfig).map(({ element, path }) => (
-          <Route
-            key={path}
-            path={path}
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <div>{element}</div>
-              </Suspense>
-            }
-          />
-        ))}
-      </Routes>
-    </Suspense>
+    <div className={c.app}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {Object.values(routeConfig).map(({ element, path }) => (
+            <Route
+              key={path}
+              path={path}
+              element={<Suspense fallback={<div>Loading...</div>}>{element}</Suspense>}
+            />
+          ))}
+        </Routes>
+      </Suspense>
+    </div>
   )
 }
+
+const useStyles = createStyles(({ colors }) => ({
+  app: {
+    background: colors.background,
+    boxSizing: 'border-box',
+    width: '100vw',
+    minHeight: '100vh',
+  },
+}))
 
 export default App
