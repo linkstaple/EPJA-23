a92094ab2293f5eb2e1805b8b70df36531fe10fa
majorro <andrabaratrum007@gmail.com>
2023-04-29 06:08:06 +0300

    added tests config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+module.exports = {
+  preset: "ts-jest/presets/js-with-ts",
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  moduleNameMapper: {},
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  testMatch: ["**/__tests__/*.(ts|tsx)"],
+  setupFilesAfterEnv: ["./jest.setup.ts"],
+  testPathIgnorePatterns: ["./.next/", "./node_modules/"],
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.jest.json",
+    },
+  },
+};
